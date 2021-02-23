b68b9427c592669ae095d982607a7875a33c056f
ZIBMBRAZIL <moreira_igor9@hotmail.com>
2021-02-23 20:36:51 -0300

    Desvendando o Next.js FIM SEGUNDO DIA

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -7,8 +7,8 @@ export function ExperienceBar() {
                 0 xp
             </span>
             <div>
-                <div style={{width:'50%'}}/>
-                <span className={styles.currentExperience} style={{left:'50%'}}>300 xp</span>
+                <div style={{ width: '50%' }} />
+                <span className={styles.currentExperience} style={{ left: '50%' }}>300 xp</span>
             </div>
             <span>
                 600 xp
