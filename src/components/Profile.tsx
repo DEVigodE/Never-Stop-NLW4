9defb978c85613d9f0a21d2c41912ce4ce8e3eaf
ZIBMBRAZIL <moreira_igor9@hotmail.com>
2021-02-24 17:41:28 -0300

    part1 da aula 3

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -2,12 +2,16 @@ import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
     return (
-        <div className={styles.profileContainer}>
-            <img src="https://github.com/ZIBMBRAZIL.png" alt="Igor Bittencourt" />
-            <div>
-                <strong>Igor Bittencourt</strong>
-                <p>Level 1</p>
-            </div>
+      <div className={styles.profileContainer}>
+        <img src="https://github.com/ZIBMBRAZIL.png" alt="Igor Bittencourt" />
+        <div>
+          <strong>Igor Bittencourt</strong>
+          
+          <p>
+            <img src="icons/level.svg" alt="Level"/>
+            Level 1
+          </p>
         </div>
-    );
-}
+      </div>
+    )
+  }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { CompletedChallenges } from '../components/CompletedChallenges'
 import { Countdown } from '../components/Countdown'
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from '../components/Profile';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import styles from '../styles/pages/Home.module.css';
 
@@ -10,7 +11,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Início | Don't STOP</title>
+        <title>Início | Never STOP</title>
       </Head>
 
       <ExperienceBar />
@@ -22,7 +23,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
