b77eaf356e3ada6ecbac1550ffe255fedc5aed62
ZIBMBRAZIL <moreira_igor9@hotmail.com>
2021-02-26 16:08:20 -0300

    Aula 4 e um pouco da aula 5

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,17 +1,22 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
-    return (
-      <div className={styles.profileContainer}>
-        <img src="https://github.com/ZIBMBRAZIL.png" alt="Igor Bittencourt" />
-        <div>
-          <strong>Igor Bittencourt</strong>
-          
-          <p>
-            <img src="icons/level.svg" alt="Level"/>
-            Level 1
-          </p>
-        </div>
+
+  const { level } = useContext(ChallengesContext)
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/ZIBMBRAZIL.png" alt="Igor Bittencourt"/>
+      <div>
+        <strong>Igor Bittencourt</strong>
+        
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level {level}
+        </p>
       </div>
-    )
-  }
+    </div>
+  )
+}
